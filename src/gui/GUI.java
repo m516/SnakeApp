a51b9ff48d4111ba28e7b6fa029fea487a4b8ab0
mm44928 <mm44928@VHS60105.wdmschools.local>
2017-05-11 10:39:36 -0500

    Minor changes 5/11

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,10 +1,9 @@
 package gui;
-import application.*;
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class GUI extends Application {
@@ -13,7 +12,7 @@ public class GUI extends Application {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("GUILayout.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());	
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
